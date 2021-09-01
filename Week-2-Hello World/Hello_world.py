cef91a9a0e5a404febdc6b92c1413fd7e44a6c0b
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-01 18:27:59 +0630

    Hello World Program

---
+++ Week-2-Hello World/Hello_world.py
@@ -0,0 +1,9 @@
+def main():
+    print('Please enter your name:')
+    user_name = input()
+    output_sentence = f"Hi {user_name}, welcome to Python!"
+    print(output_sentence)
+
+
+if __name__ == '__main__':
+    main()
